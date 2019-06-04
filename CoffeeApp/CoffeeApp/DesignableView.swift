7abb28c2a8e3408162c2b63c9881d90a65d23e58
Günther Holz Ribak <gribak@outdabox.in>
2019-06-03 22:01:59 -0300

    feat(onboarding): onboarding screen implemented.

---
+++ CoffeeApp/CoffeeApp/DesignableView.swift
@@ -0,0 +1,12 @@
+//
+//  DesignableView.swift
+//  CoffeeApp
+//
+//  Created by Gunther Ribak on 03/06/2019.
+//  Copyright © 2019 CWI Software. All rights reserved.
+//
+
+import UIKit
+
+@IBDesignable
+class DesignableView: UIView {}
