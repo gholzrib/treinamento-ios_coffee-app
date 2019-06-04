9acd61e7eab3fecc6f8e0f4d5aeb9564dd3a2267
Günther Holz Ribak <gribak@outdabox.in>
2019-06-04 11:44:51 -0300

    feat(*): navigation added. login screen implemented.

---
+++ CoffeeApp/CoffeeApp/LoginViewController.swift
@@ -0,0 +1,18 @@
+//
+//  LoginViewController.swift
+//  CoffeeApp
+//
+//  Created by Gunther Ribak on 03/06/2019.
+//  Copyright © 2019 CWI Software. All rights reserved.
+//
+
+import UIKit
+
+class LoginViewController: UIViewController {
+
+    @IBAction func onBackPressed(_ sender: UIButton) {
+        self.navigationController?.popViewController(animated: true)
+    }
+    
+
+}
